bb419ad34acc5fbe7e3834df91363f1a98ac59ee
aaravhajare <hajareaarav66@gmail.com>
2025-04-20 09:39:01 +0530

    Class Vehicle

---
+++ Class Vehicle.py
@@ -0,0 +1,12 @@
+
+class vehicle:
+
+    def __init__(self,speed,milege):
+
+        self.speed = speed
+        self.milege =  milege
+
+modlex = vehicle(240,15)
+
+print("speed = " , modlex.speed)
+print("milege = " , modlex.milege)
